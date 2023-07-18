be14925b66d5c6ced4fe344feb4bed8a051266d6
hunggahot <hunggahot@gmail.com>
2023-07-18 20:55:10 +0700

    axios example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import productApi from "./api/productApi";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [result, setResults] = useState([]);
+  const [searchInfo, setSearchInfo] = useState({});
+  const [product, setProducts] = useState([]);
+
+  // useEffect(() => {
+  //   if (search === "") return;
+  //   axios.get(
+  //     `https://en.wikipedia.org/w/api/php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${search}`
+  //   );
+  // });
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const params = {
+        _limit: 10,
+      };
+      try {
+        const { data } = await productApi.getAll(params);
+        setProducts(data);
+      } catch (err) {
+        if (err.response) {
+          // Notthing in the 200 response range
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else {
+          console.log(`Error: ${err.message}`);
+        }
+      }
+    };
+
+    fetchProducts();
+
+    console.log(product, "product");
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Wikipedia API</h1>
+
+      {/* {product.map((item) => {
+        return <h4>{item.name}</h4>;
+      })} */}
     </div>
   );
 }
